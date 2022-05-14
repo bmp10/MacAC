a352f7b233b98593de9b695e74bd6fc4586969e8
bmp10 <89558241+bmp10@users.noreply.github.com>
2022-05-13 22:51:28 -0700

    Add files via upload

---
+++ autoclicker.py
@@ -0,0 +1,25 @@
+import pynput
+from time import sleep
+
+click = False
+
+mouse = pynput.mouse.Controller()
+
+def keydown(key):
+    global click
+
+    try:
+        key = key.char
+    except:
+        fill = ''
+
+    if key == 'z' or key == 'Z':
+        click = not click
+
+pynput.keyboard.Listener(on_press = keydown).start()
+
+while True:
+    if click:
+        mouse.click(pynput.mouse.Button.left)
+        
+        sleep(0.1)
